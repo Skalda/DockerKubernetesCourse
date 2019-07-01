33e35ef54070c072ddef45cb0758e026373c10a0
Tomas Skalicky <skalicky.tomas@hotmail.com>
2019-07-01 21:47:07 +0200

    Nginx config + fixes

--- complex/client/src/App.js
+++ complex/client/src/App.js
@@ -13,11 +13,13 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-        </header>
+
+
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
+        </header>
       </div>
     </Router>
   );
